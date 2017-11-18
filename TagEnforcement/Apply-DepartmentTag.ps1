e74615baf1e9f8084292946abc9795919ffbb61e
erleonard <erleonard@outlook.com>
2017-11-17 21:57:18 -0500

    update

--- TagEnforcement/Apply-DepartmentTag.ps1
+++ TagEnforcement/Apply-DepartmentTag.ps1
@@ -1,8 +1,11 @@
 #Authenticate to Azure
 Login-AzureRmAccount
 
+#Get azure subscription information
+$Subscription = Get-AzureRmSubscription
+
 #Create new Azure Policy Definition
-$policy = New-AzureRmPolicyDefinition -Name DepartmentPolicyDefinition -Description "Policy to deny resource without Department tag" -Policy "C:\scripts\AzureSecurityPolicy\DepartmentPolicyDefinition.json"
+$policy = New-AzureRmPolicyDefinition -Name DepartmentPolicyDefinition -Description "Policy to deny resource without Department tag" -Policy "https://raw.githubusercontent.com/erleonard/AzurePolicy/master/TagEnforcement/EnforceDepartmentTag.json"
 
 #Apply to subscription
 New-AzureRmPolicyAssignment -Name DepartmentPolicyAssignment -Scope /subscriptions/$Subscription -PolicyDefinition $policy
